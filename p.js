c6d96fa4d6dba36e89faf42dd5792aa83f0c465a
fafw3effe <aaditayshah1555@gmail.com>
2025-04-23 23:54:53 +0545

    a

--- p.js
+++ p.js
@@ -20,7 +20,7 @@
   */
 
   {
-    title:'Peaky Blinders [Season 2] [Episode 1] WEB-DL [Hindi + English] [HEVC]',
+    title:'Peaky Blinders [Season 1] [Episode 1] WEB-DL [Hindi + English] [HEVC]',
     im:'vPlsKun2WZEsqe3yZTmebxtwElW',
     dl:'https://pub-db4aad121b26409eb63bf48ceb693403.r2.dev/Peaky.Blinders.S01.E01.720p.10Bit.HEVC.BluRay.Hindi.2.0-English.2.0.ESub.x265-HDHub4u.Ms.mkv',
     link: 'tt2442560'

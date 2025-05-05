e97d43e2b722c26ae26aac2029cb8f361f0cd227
fafw3effe <aaditayshah1555@gmail.com>
2025-05-05 19:39:41 +0545

    aa

---
+++ am.js
@@ -0,0 +1,47 @@
+const movies = [ 
+    {
+        "title": "The Last of Us [Season 2] [Episode 4] WEB-DL [Hindi + English] | HBO Series",
+        "im": "dmo6TYuuJgaYinXBPjrgG9mB5od",
+        "bgi": "hI5PALWKjn9DeyQkAiObJwzSeOz",
+        "dl": "https://pixeldrain.net/api/file/r7SyvyhS?download",
+        "link": "tt3581920",
+        "year": "2025",
+        "duration": "60 min",
+        "cast": "Pedro Pascal, Bella Ramsey, Kaitlyn Dever",
+        "rating": "9.3",
+        "genre": "Action, Adventure, Drama",
+        "description": "The Last of Us follows Joel and Ellie as they navigate a post-apocalyptic world. In Season 2, the journey becomes even more perilous as the two continue to face new threats and uncover shocking truths.",
+        "type": "series",
+        "quality": "webdl"
+    },
+    {
+        "title": "Peaky Blinders [Season 2] [Episode 2] WEB-DL [Hindi + English]",
+        "im": "m4JPtC3gWUsq0GlfNK3SdgkL9XG",
+        "bgi": "yQUuWCeTkuhZrXEKcEtMWisoCwO",
+        "dl": "https://pixeldrain.net/api/file/QkeMSRdR?download",
+        "link": "tt2442560",
+        "year": "2013",
+        "duration": "58 min",
+        "cast": "Cillian Murphy, Sam Neill, Helen McCrory",
+        "rating": "8.8",
+        "genre": "Crime, Drama",
+        "description": "A notorious gang in 1919 Birmingham, England, is led by the fierce Tommy Shelby, a crime boss set on moving up in the world no matter the cost.",
+        "type": "series",
+        "quality": "webdl"
+    },
+    {
+        "title": "Dragon [2025] WEB-DL [Hindi + Tamil] | Full Movie",
+        "im": "3BR3YTR04dO9ztCx8LZx2C2eu4G",
+        "bgi": "8bhvwNGOzUHUbtZ52NEseQnWtEH",
+        "dl": "https://pixeldrain.net/api/file/Rt52vXzB?download",
+        "link": "tt32080876",
+        "year": "2025",
+        "duration": "110 min",
+        "cast": "Aishwarya Rai, Vijay Sethupathi, Tovino Thomas",
+        "rating": "7.2",
+        "genre": "Action, Fantasy, Drama",
+        "description": "In a battle between humans and dragons, a young warrior must fight to save her homeland from a devastating war that could destroy everything she holds dear.",
+        "type": "movie",
+        "quality": "webdl"
+    }
+];

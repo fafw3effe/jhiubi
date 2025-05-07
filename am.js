b72a8c85ea22ebbaf990ac31d9c54edd837ae117
fafw3effe <aaditayshah1555@gmail.com>
2025-05-07 16:38:38 +0545

    aa

--- am.js
+++ am.js
@@ -21,10 +21,10 @@ const movies = [
     */
 
     {
-        "title": "Last Bullet [2025] WEB-DL [Hindi + English] [HEVC] | Full Movie",
+        "title": "Last Bullet [2025] WEB-DL [Hindi + English] | Full Movie",
         "im": "gO33EBU7JHd6xBjT62XMt61KoqX",
         "bgi": "1ikqGTVjXA9wkDsESVVzpLP8H1r",
-        "dl": "https://pub-ed0ca22af5c04301b7abe9dac4aaf9bc.r2.dev/Last.Bullet.2025.720p.NF.WEB-DL.DUAL.DDP5.1.H265-Vegamovies.is.mkv",
+        "dl": "https://pixeldrain.net/api/file/vDeetui6?download",
         "year": "2025",
         "duration": "1h 51m",
         "rating": "7.0",

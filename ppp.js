fc06b889ec540be7f871cfbab5df1ccaaa579cea
fafw3effe <aaditayshah1555@gmail.com>
2025-05-10 00:36:38 +0545

    aaaa

--- ppp.js
+++ ppp.js
@@ -173,8 +173,12 @@ function displayMovies() {
         if (movie.type === 'series') {
             const seasonMatch = movie.title.match(/\[Season (\d+)\]/);
             const seasonText = seasonMatch ? `Season ${seasonMatch[1]}` : 'Season 1';
+            
+            const seasonMatche = movie.title.match(/\[Episode (\d+)\]/);
+            const seasonTexte = seasonMatche ? `Episode ${seasonMatche[1]}` : '';
+
             sticker.className = 'sticker season-sticker';
-            sticker.textContent = seasonText;
+            sticker.textContent = seasonText+seasonTexte;
         } else {
             sticker.className = `sticker quality-sticker ${qualityClass}`;
             sticker.textContent = qualityText.toUpperCase();

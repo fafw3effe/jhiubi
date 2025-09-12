85aad4898d07b71f9715b1ee08cbf9bde08e0f67
fafw3effe <aaditayshah1555@gmail.com>
2025-09-12 08:56:43 -0700

    aa

---
+++ app.js
@@ -0,0 +1,18 @@
+[
+  {
+    "link": "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    "type": "youtube"
+  },
+  {
+    "link": "https://www.tiktok.com/embed/7221234567890123456",
+    "type": "tiktok"
+  },
+  {
+    "link": "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/facebook/videos/10153231379946729/",
+    "type": "facebook"
+  },
+  {
+    "link": "https://www.instagram.com/reel/Cx12345/embed/",
+    "type": "instagram"
+  }
+]
